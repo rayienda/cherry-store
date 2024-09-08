d0d91b48183f81f2b0e6ef9bebb53ed64ce88aa2
RAYIENDA <rayienda18@gmail.com>
2024-09-08 21:19:40 +0700

    Updated main

--- main/models.py
+++ main/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-class StoreInfo(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-
-    @property
-    def is_mood_strong(self):
-        return self.price > 5
--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'product' : 'Cherry Heels',
-        'price': '270.000',
-        'description': 'Very comfortable and stylish for everyday use.'
+        'application_name': 'cherry-shop',
+        'class': 'PBD KKI',
+        'name': 'Rayienda Hasmaradana',
     }
 
     return render(request, "main.html", context)
